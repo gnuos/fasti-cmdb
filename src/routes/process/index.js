2e7b07043af20004d22ea6e7ce09b144afcab61b
Kevin <gnuos@sina.cn>
2021-04-21 11:01:01 +0800

    修复了管理进程的接口同步执行shell命令的问题

--- src/routes/process/index.js
+++ src/routes/process/index.js
@@ -1,50 +1,65 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 /* eslint-disable func-names */
-const { exec } = require('child_process');
-
-async function shell(cmd) {
-  exec(`${cmd}`, (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-  });
-}
+const { spawnSync } = require('child_process');
 
 module.exports = async function (fastify, options) {
   fastify.post('/gobetween', async (request, reply) => {
     const data = request.body;
+    const cmd = '/usr/bin/supervisorctl';
+
     let msg = {};
 
-    if (!data.action || !(data.action instanceof String)) {
+    const shell = (args) => {
+      const result = spawnSync(`${cmd}`, args);
+
+      if (result.error) {
+        console.log(result.error);
+
+        msg = { code: 500, status: 'failed', msg: result.error };
+        return;
+      }
+
+      if (result.stderr.constructor === String || result.stderr[0]) {
+        console.log(`stderr: ${result.stderr}`);
+        msg = { code: 200, status: 'failed', msg: result.stderr };
+        return;
+      }
+
+      if (result.status !== undefined && result.status !== null && result.status === 0) {
+        console.log(`stdout: ${result.stdout}`);
+      } else {
+        console.log(`status: ${result.status}`);
+        console.log(`stdout: ${result.stdout}`);
+        msg = { code: 200, status: 'failed', msg: `${data.action} process gobetween failed` };
+      }
+    };
+
+    if (data.action === undefined || data.action === null || !(data.action.constructor === String)) {
       msg = {
         code: 400,
         status: 'failed',
         msg: 'ERROR: need a action',
       };
-      reply.send(msg);
+    } else {
+      msg = { code: 200, status: 'success', msg: `${data.action} process gobetween finished` };
+
+      switch (data.action) {
+        case 'restart':
+          shell(['restart', 'portmap']);
+          break;
+        case 'start':
+          shell(['start', 'portmap']);
+          break;
+        default:
+          msg = {
+            code: 400,
+            status: 'failed',
+            msg: `ERROR: undefined ${data.action} action`,
+          };
+      }
     }
 
-    switch (data.action) {
-      case 'restart':
-        shell('/usr/bin/supervisorctl restart portmap');
-        break;
-      case 'start':
-        shell('/usr/bin/supervisorctl start portmap');
-        break;
-      default:
-        msg = {
-          code: 400,
-          status: 'failed',
-          msg: `ERROR: undefined ${data.action} action`,
-        };
-        reply.send(msg);
-    }
+    return msg;
   });
 };
